d0da7741af658cc9cbdb3acfd9b834e010e0daba
Mark Vavilov <Mark.Vavilov@jetbrains.com>
2021-10-29 19:30:38 +0300

    add test for ranked poll

--- src/test/kotlin/com/hse/rpp/RankedPollTest.kt
+++ src/test/kotlin/com/hse/rpp/RankedPollTest.kt
@@ -22,16 +22,25 @@ class RankedPollTest {
     @Test
     fun `multivote test`(){
         val poll = RankedPoll("asd", "dsa", "sda")
-        poll.addVote(listOf<Int>(1, 0, 2))
-        poll.addVote(listOf<Int>(1, 2, 0))
+        poll.addVote(listOf(1, 0, 2))
+        poll.addVote(listOf(1, 2, 0))
         assertEquals("dsa", poll.winner())
     }
 
     @Test
     fun `multivote test 2`() {
         val poll = RankedPoll("asd", "dsa", "sda")
-        poll.addVote(listOf<Int>(2, 0, 1))
-        poll.addVote(listOf<Int>(2, 1, 0))
+        poll.addVote(listOf(2, 0, 1))
+        poll.addVote(listOf(2, 1, 0))
         assertEquals("sda", poll.winner())
+    }
+
+    @Test
+    fun `multivote test 3`() {
+        val poll = RankedPoll("asd", "dsa", "sda")
+        poll.addVote(listOf(0, 1, 2))
+        poll.addVote(listOf(1, 0, 2))
+        poll.addVote(listOf(2, 1, 0))
+        assertEquals("dsa", poll.winner())
     }
 }
