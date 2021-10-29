54c00173df91eb2cab60e073164409ada46bcbba
gt22 <igorengel@mail.ru>
2021-10-29 17:34:55 +0300

    new test

--- src/test/kotlin/com/hse/rpp/RankedPollTest.kt
+++ src/test/kotlin/com/hse/rpp/RankedPollTest.kt
@@ -26,4 +26,12 @@ class RankedPollTest {
         poll.addVote(listOf<Int>(1, 2, 0))
         assertEquals("dsa", poll.winner())
     }
+
+    @Test
+    fun `multivote test 2`() {
+        val poll = RankedPoll("asd", "dsa", "sda")
+        poll.addVote(listOf<Int>(2, 0, 1))
+        poll.addVote(listOf<Int>(2, 1, 0))
+        assertEquals("dsa", poll.winner())
+    }
 }
