66b7cecefa6efb9eed55663284acfec636b13974
shirokov-kirill <k.s.shirokov@mail.ru>
2021-11-06 20:44:53 +0300

    add test

--- src/test/kotlin/com/hse/rpp/ProbabilityPollTest.kt
+++ src/test/kotlin/com/hse/rpp/ProbabilityPollTest.kt
@@ -11,4 +11,11 @@ class ProbabilityPollTest {
         poll.addVoteForFirst(0.6)
         assertEquals(0.6, poll.firstProb())
     }
+
+    @Test
+    fun `single vote poll 2`() {
+        val poll = ProbabilityPoll("true", "false")
+        poll.addVoteForFirst(0.7)
+        assertEquals(0.7, poll.firstProb())
+    }
 }
